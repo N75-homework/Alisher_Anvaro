672613b8e9fbf3cffe84abab5b9f9c6fcc0e6df7
AnvarovAN <alianvarov85@gmail.com>
2022-11-06 00:45:55 +0500

    added first task and full OTM

--- script/app.js
+++ script/app.js
@@ -1,4 +1,76 @@
-//  2.task
+//  2.Задачи
+        // // 1-задача
+        // var numb = -12;
+        // if(numb > 0){
+        //     console.log("число-положительное");
+        // } else{
+        //     console.log("число-отрицательное");
+            
+        // }
+        
+        // // 2-Задача
+
+        // var num = 9
+
+        // if (num % 2 == 0 ){
+        //     console.log("Число четное");
+        // } else{
+        //     console.log(" Число нечетное");
+        // }
+
+
+        // // 3-задача
+        // var a = 4;
+        // var b = 3;
+        
+        // if(a > 0 && b > 0){
+        //         console.log(`${a} > 0 и ${b} > 0`);
+        // } else {
+        //         console.log("False");
+
+        // }
+
+
+
+        // 4-задача
+       //  var a = -1;      
+       //  var b = 3;
+       //  var c = 6;
+
+       //  if(a < b && c > b && a < c){
+       //         console.log( a, b, c); 
+       //  }else if(b > c){
+       //         console.log("False (b больше c)");
+       //  }else if(a > b){
+       //         console.log("False(a больше b )");
+       //  }else if(a == c && a == b && b == c) {   
+       //         console.log("False(введите другие числа)");
+       //  }else {
+       //         console.log("b равно с");
+       //  }
+        
+       //  5-задача.
+       // let a = 12,
+       //  b = -13,
+       //  c = -14,
+       //  d = -15;
+       // if(a < 0 && b < 0 && c < 0 && d < 0 ){
+       //  console.log(a, b, c, d);
+       // }else if(a > 0){
+       //  console.log(`${a} - положительное число`, b, c,d);
+       
+       // }else if(b > 0){
+       //  console.log(a, `${b} - положительное число`, c, d);
+       
+       // }else if(c > 0){
+       //  console.log(a, b,  `${c} - положительное число`, d);
+       
+       // }else if(d > 0){
+       //  console.log(a, b, c, `${d} - положительное число`,);
+       // }else {
+       //    console.log("проверка");
+       // }
+    
 
 
 
@@ -6,46 +78,43 @@
 
 
 
+// 3. OTM Задание
+// const eco = 'TDIU',
+// fin = 'TDMI',
+// nat = 'MU'
 
+// let name = prompt('Ismingizni kiriting'),
+// otm = prompt(`${name},qaysi OTM tanlaysiz:
+// 1.${eco}
+// 2.${fin}
+// 3.${nat}
+// PS: Nomini misoldagidek kiriting :)
+// `),
+// addScore = prompt(`${name}, toplagan balingizni kiriting`);
 
-
-// 3. OTM TASK
-const eco = 'TDIU',
-fin = 'TDMI',
-nat = 'MU'
-
-let name = prompt('Ismingizni kiriting'),
-otm = prompt(`${name},qaysi OTM tanlaysiz:
-1.${eco}
-2.${fin}
-3.${nat}
-PS: Nomini misoldagidek kiriting :)
-`),
-addScore = prompt(`${name}, toplagan balingizni kiriting`);
-
-if(otm != eco && otm != fin && otm != nat ) {
-    alert(`OTM nomini togri kiriting:)`)
-}else if(otm === eco && addScore > 150){
-    alert(`Siz ${eco} ga grantga kirdiz `)
-}else if( otm === eco && addScore > 120 && addScore < 150){
-    alert(`Siz ${eco} ga kontraktga  kirdiz `)
-}else if( otm === eco && addScore < 120){
-    alert(`Sizni ballingiz ${eco}ga etmadi. Siz yiqildiz `)
-}else if(otm === fin && addScore > 160){
-    alert(`Siz ${fin} ga grantga kirdiz `)
-}else if( otm === fin && addScore > 100 && addScore < 160){
-    alert(`Siz ${fin} ga kontraktga  kirdiz `)
-}else if( otm === fin && addScore < 100){
-    alert(`Sizni ballingiz ${fin}ga etmadi. Siz yiqildiz `)
-}else if(otm === nat && addScore > 180){
-    alert(`Siz ${nat} ga grantga kirdiz `)
-}else if( otm === nat && addScore > 168 && addScore < 180){
-    alert(`Siz ${nat} ga kontraktga  kirdiz `)
-}else if( otm === nat && addScore < 168){
-    alert(`Sizni ballingiz ${nat} ga etmadi. Siz yiqildiz `)
-}else if(typeof(addScore) != 'Number'){
-    alert('Son kiriting')
-};
+// if(otm != eco && otm != fin && otm != nat ) {
+//     alert(`OTM nomini togri kiriting:)`)
+// }else if(otm === eco && addScore > 150){
+//     alert(`Siz ${eco} ga grantga kirdiz `)
+// }else if( otm === eco && addScore > 120 && addScore < 150){
+//     alert(`Siz ${eco} ga kontraktga  kirdiz `)
+// }else if( otm === eco && addScore < 120){
+//     alert(`Sizni ballingiz ${eco}ga etmadi. Siz yiqildiz `)
+// }else if(otm === fin && addScore > 160){
+//     alert(`Siz ${fin} ga grantga kirdiz `)
+// }else if( otm === fin && addScore > 100 && addScore < 160){
+//     alert(`Siz ${fin} ga kontraktga  kirdiz `)
+// }else if( otm === fin && addScore < 100){
+//     alert(`Sizni ballingiz ${fin}ga etmadi. Siz yiqildiz `)
+// }else if(otm === nat && addScore > 180){
+//     alert(`Siz ${nat} ga grantga kirdiz `)
+// }else if( otm === nat && addScore > 168 && addScore < 180){
+//     alert(`Siz ${nat} ga kontraktga  kirdiz `)
+// }else if( otm === nat && addScore < 168){
+//     alert(`Sizni ballingiz ${nat} ga etmadi. Siz yiqildiz `)
+// }else if(typeof(addScore) != 'Number'){
+//     alert('Son kiriting')
+// };
 
 
 
