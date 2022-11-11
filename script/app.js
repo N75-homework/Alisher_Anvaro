dd06738f9008a1275dddcb75e38d2dd62759c83d
AnvarovAN <alianvarov85@gmail.com>
2022-11-11 12:36:47 +0500

    counter

--- script/app.js
+++ script/app.js
@@ -1,43 +1,30 @@
-let img = prompt('Paste link to the pic'),
-name = prompt('Enter your name'),
-lastName = prompt('Enter your lastname'),
-email = prompt('Enter your email'),
-tel = prompt('Enter your phone number', 998),
-add = prompt('Enter your  address'),
-edu = prompt('What school did you go to'),
-durOfedu = prompt('Your education period at this school. Ex: 2010-2021 or 2012-current'),
-heu = prompt('Which university you finished'),
-durOfHeu = prompt('Your education period at this university.'),
-work = prompt('Where do you work'),
-durOfWork = prompt('How long have you worked there')
-top1.innerHTML = 
-`
-<h2 class="top1__name"> ${name} <br> ${lastName}</h2>
-<a href=""  class="icon top1__tel"><i class="fa-solid fa-phone"></i> :  ${tel}</a>
-<a href=""  class="icon top1__email"><i class="fa-solid fa-envelope"></i> :  ${email}</a>
-<a href=""  class="icon top1__add"><i class="fa-solid fa-location-dot"></i> :  ${add}</a>
-`
-top2.innerHTML = 
-`
-<img class="img" src="${img}" alt="">
-`
+const btns = document.querySelectorAll(".btn")
+const count = document.querySelector(".count")
 
-edu1.innerHTML = 
-`
-<p class="title"> ${durOfedu}</p>
-<p class="subtitle"> ${edu}</p>
-`
+let sum = 0 
 
-edu2.innerHTML = 
-`
-<p class="title"> ${durOfHeu}</p>
-<p class="subtitle">${heu}</p>
+btns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        const styles = e.currentTarget.classList
+        if(styles.contains("increase")){
+            sum++
+        } else   if(styles.contains("decrease")){
+            sum--
+        } else {
+            sum = 0
+        }
+        if(sum < 0){
+            count.style.color = 'red'
+        }
+        if(sum === 0){
+            count.style.color = 'black'
+        }
+        if(sum > 0){
+            count.style.color = 'green'
+        }
+        count.textContent = sum
+    })
 
-`
-exp1.innerHTML = 
-`
-<p class="title"> ${durOfWork}</p>
-<p class="subtitle"> ${work}</p>
-`
+})
 
 
