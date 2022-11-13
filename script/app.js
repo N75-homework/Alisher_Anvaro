b989f81303818b04dbb6243ca4b57aa02bce023e
AnvarovAN <alianvarov85@gmail.com>
2022-11-14 04:46:25 +0500

    full vts

--- script/app.js
+++ script/app.js
@@ -1,30 +1,54 @@
-const btns = document.querySelectorAll(".btn")
-const count = document.querySelector(".count")
+const piyoda = document.querySelector("#piyoda");
+const bike = document.querySelector("#velik");
+const car = document.querySelector("#car");
+const plane = document.querySelector("#plane");
+const inp = document.querySelector("#km")
+const form = document.querySelector("#form")
 
-let sum = 0 
 
-btns.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-        const styles = e.currentTarget.classList
-        if(styles.contains("increase")){
-            sum++
-        } else   if(styles.contains("decrease")){
-            sum--
-        } else {
-            sum = 0
+form.addEventListener("click",function(e) {
+    e.preventDefault();
+    if(inp.value < 0){
+        alert('Введиет положительное число:)')
+        inp.value = 0;
+
+    }
+    function vts(data){
+        let time = inp.value / data,
+        d = time / 24,
+        h = time - Math.floor(d )* 24,
+        m = (h * 60) - Math.floor(h )* 60,
+        s = (m * 60) - Math.floor(m )* 60;
+        day = Math.floor(d);
+        hour = Math.floor(h);
+        minute = Math.floor(m);
+        second = Math.floor(s);
+        
+        return include (day, hour, minute, second);
+    }
+
+    function include (day, hour, minute, second){
+        if(day < 1){
+            return `${hour}:${minute}:${second}`
+        }else{
+            return `${day}-day ${hour}:${minute}:${second}`
         }
-        if(sum < 0){
-            count.style.color = 'red'
-        }
-        if(sum === 0){
-            count.style.color = 'black'
-        }
-        if(sum > 0){
-            count.style.color = 'green'
-        }
-        count.textContent = sum
-    })
+        
+    }
+    
+piyoda.textContent = vts(3.6);
+bike.textContent = vts(14);
+car.textContent = vts(80);
+plane.textContent = vts(800);
+
 
 })
 
 
+
+
+// Время = 842 км / 67 км/ч = 12,5672 часа
+
+// Преобразуем эту десятичную дробь в минуты и секунды. Во-первых, умножьте 0,5672 на 60 = 34,0299 минут. Теперь мы умножаем это десятичное число на 60, чтобы получить секунды: 0,0299 * 60 = 1,8 секунды. Мы можем округлить это до 2 секунд, что даст нам 12 часов 34 минуты и 2 секунды.
+
+// Теперь вы знаете, как сделать преобразование скорости, расстояния и времени.
