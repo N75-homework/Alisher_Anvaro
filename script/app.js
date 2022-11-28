a08bd860a99405809a051f2783aa3b75bad69eab
AnvarovAN <alianvarov85@gmail.com>
2022-11-28 13:28:02 +0500

    light

--- script/app.js
+++ script/app.js
@@ -1,180 +1,39 @@
-// 1.Задачи(
-// let a = prompt(`введите длиину в см`)
-// let result = a / 100;
-// alert(result + 'метр')
-
-
-
-
-// let m = prompt(`введиет массу в кг`)
-// let result = m / 1000;
-// alert(result + 'тонн')
-
-
-
-
-
-// let a = prompt(`Son kiriting`),
-// b = a%10,
-// c = (a - b) / 10.
-// alert(` ${a} soni ${c} onlik va ${b} birliklardan iborat`)
-
-
-
-
-// let a = prompt(`Son kiriting`),
-// c = ((a - a%10) / 10) + a%10
-// alert(`${a} sonini raqamlar yigindisi ${c} ga`)
-
-
-
-
-// let a = prompt(`Son kiriting`),
-// b = a%10,
-// c = (a - b) / 10,
-// d = b * c;
-// alert(`${a} sonini raqamlar yigindisi ${d} ga`)
-
-
-
-
-// let a = prompt('Вdедите 4х значное число'),
-// b = (a - a%100) / 100
-// alert(`число ${a} содержит ${b} сотен(ни)`);
-
-
-
-// var a = prompt('Kun boshidan necha sekund otdi');
-// var b = a / 60;
-// var c = a / 3600; 
-// alert(`Kun boshidan ${b} minut, ${c} soat otdi`);
-
-
-
-
-//  2.Задачи
-
-
-
-        // 1-задача
-        // let numb = prompt(`Введите число`);
-        // if(numb > 0){
-        //     alert("число-положительное");
-        // } else{
-        //     alert("число-отрицательное");
-            
-        // }
+let body = document.querySelector('body')
+let btn = document.querySelector('#btn')
+let rec = document.querySelector('.rec')
+let circle = document.querySelectorAll('.circle')
+let red = document.querySelector('.red')
+let grn = document.querySelector('.green')
+let ylw = document.querySelector('.yellow')
+var recognizer = new webkitSpeechRecognition();
+recognizer.interimResults = true;
+recognizer.lang = 'en-Us';
+circle.forEach((item)=>{
+    console.log(item);
+})
+    recognizer.onresult = function (event) {
         
-        // // 2-Задача
+  var result = event.results[event.resultIndex];
+  if (result.isFinal) {
+    body.style.backgroundColor = result[0].transcript
+    rec.textContent = result[0].transcript
+    if(result[0].transcript === 'yellow'){
+        ylw.style.background = result[0].transcript
+    }else    if(result[0].transcript === 'green'){
+      grn.style.background = result[0].transcript
+  }else    if(result[0].transcript === 'red'){
+    red.style.background = result[0].transcript
+}
 
-        // let num = prompt(`Введите любое число`)
-
-        // if (num % 2 == 0 ){
-        //     alert("Число четное");
-        // } else if(typeof(num) != 'number'){
-        //         alert (`Введите число`)
-        // }else {
-        //         alert(" Число нечетное");
-        // }
-
-
-
-
-        
-        // 3-задача
-        // let a = prompt(`Введите а`),
-        //  b = prompt(`Введите b`);
-        
-        // if(a > 0 && b > 0){
-        //         alert(`${a} > 0 и ${b} > 0`);
-        // } else {
-        //         alert("False");
-
-        // }
-
-
-
-
-        // 4-задача
-        // let a = prompt(`введите a`);
-        // let b = prompt(`введите b`);
-        // let c = prompt(`введите c`);
-
-        // if(a < b && c > b && a < c){
-        //        alert(`${a} ${b} ${c}`); 
-        // }else if(b > c){
-        //        alert("False (b больше c)");
-        // }else if(a > b){
-        //        alert("False(a больше b )");
-        // }else if(a == c && a == b && b == c) {   
-        //        alert("False(введите другие числа)");
-        // }else{
-        //        alert("b равно с");
-        // }
-        
-
-
-       //  5-задача.
-      //   let a = prompt(`введите a`);
-      //    b = prompt(`введите b`),
-      //    c = prompt(`введите c`),
-      //    d = prompt(`введите d`);
-      //  if(a < 0 && b < 0 && c < 0 && d < 0 ){
-      //   alert(`${a}, ${b}, ${c}, ${d}`);
-      //  }else {
-      //     alert(false);
-      //  }
-    
-
-
-
-
-
-
-// 3. OTM Задание
-const eco = 'TDIU',
-fin = 'TDMI',
-nat = 'MU'
-
-let name = prompt('Ismingizni kiriting'),
-otm = prompt(`${name},qaysi OTM tanlaysiz:
-1.${eco}
-2.${fin}
-3.${nat}
-PS: Nomini misoldagidek kiriting :)
-`),
-addScore = prompt(`${name}, toplagan balingizni kiriting`);
-
-if(otm != eco && otm != fin && otm != nat ) {
-    alert(`OTM nomini togri kiriting:)`)
-}else if(otm === eco && addScore > 150){
-    alert(`Siz ${eco} ga grantga kirdiz `)
-}else if( otm === eco && addScore > 120 && addScore < 150){
-    alert(`Siz ${eco} ga kontraktga  kirdiz `)
-}else if( otm === eco && addScore < 120){
-    alert(`Sizni ballingiz ${eco}ga etmadi. Siz yiqildiz `)
-}else if(otm === fin && addScore > 160){
-    alert(`Siz ${fin} ga grantga kirdiz `)
-}else if( otm === fin && addScore > 100 && addScore < 160){
-    alert(`Siz ${fin} ga kontraktga  kirdiz `)
-}else if( otm === fin && addScore < 100){
-    alert(`Sizni ballingiz ${fin}ga etmadi. Siz yiqildiz `)
-}else if(otm === nat && addScore > 180){
-    alert(`Siz ${nat} ga grantga kirdiz `)
-}else if( otm === nat && addScore > 168 && addScore < 180){
-    alert(`Siz ${nat} ga kontraktga  kirdiz `)
-}else if( otm === nat && addScore < 168){
-    alert(`Sizni ballingiz ${nat} ga etmadi. Siz yiqildiz `)
-}else if(typeof(addScore) != 'Number'){
-    alert('Son kiriting')
+  } else {
+    console.log('Промежуточный результат: ', result[0].transcript);
+  }
 };
+// console.log(result[0].transcript);
 
 
-
-
-
-
-
+recognizer.start();
+// console.log(recognizer.start());
 
 
 
